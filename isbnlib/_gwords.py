618ac74e288c306efc0ba83b28f1bdb224e3100e
ericluo04 <39772787+ericluo04@users.noreply.github.com>
2024-11-18 19:46:26 -0500

    Update _gwords.py

--- isbnlib/_gwords.py
+++ isbnlib/_gwords.py
@@ -6,8 +6,33 @@ from urllib.parse import quote
 
 from ._core import get_canonical_isbn, get_isbnlike
 from .dev import cache, webservice
+# scraping
+from bs4 import BeautifulSoup
+import requests, random
+import collections as c
 
 LOGGER = logging.getLogger(__name__)
+
+
+# extract plausible user agents (https://gist.github.com/6aditya8/c8ff33d6fc0c11de839bd9facf175cb6)
+## scrape for a certain browser
+def by_browser(browser):
+    url = 'http://www.useragentstring.com/pages/useragentstring.php?name=' + browser
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    user_agent_links = soup.find('div', {'id': 'liste'}).findAll('a')[20:40]
+    return [str(user_agent.text) for user_agent in user_agent_links]
+## compile user agents
+def get_user_agents():
+    user_agents = []
+    for browser in ['Chrome', 'Firefox', 'Mozilla', 'Safari', 'Edge', 'Internet Explorer', 'Opera']:
+        user_agents.extend(by_browser(browser))
+    return user_agents[3:] # Remove the first 3 Google Header texts from Chrome's user agents
+# find user agents
+proxy_user_agents = get_user_agents()
+# remove invalid agents
+user_agents = [i for i in proxy_user_agents if "More" not in i]
+user_agents = [i.strip() for i in user_agents if len(i) >= 10]
 
 
 @cache
@@ -16,7 +41,7 @@ def goos(words):
     service_url = 'http://www.google.com/search?q=ISBN+'
     search_url = service_url + quote(words.replace(' ', '+'))
 
-    user_agent = 'w3m/0.5.3'
+    user_agent = random.choice(user_agents)
 
     content = webservice.query(
         search_url,
